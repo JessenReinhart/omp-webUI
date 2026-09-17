5d160eca4f7413ff2f164c9d6e6e7b394504972e
jessen reinhart <jessen_1206@yahoo.com>
2026-09-17 18:07:00 +0700

    feat(ui): wire boot sequence and revamp layer

--- src/web/main.tsx
+++ src/web/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import { BootSequence } from "./BootSequence";
 import "./styles.css";
+import "./revamp.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <BootSequence />
     <App />
   </StrictMode>,
 );
